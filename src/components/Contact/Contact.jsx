50a21165f1eeae7bab886541452282776fabfdc7
VMG <victor.garcia@maticapartners.com>
2020-04-24 10:21:48 +0200

    fix the form submit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -14,75 +14,79 @@ function encode(data) {
     .join('&');
 }
 
-const Contact = () => {
-  const { contact } = useContext(PortfolioContext);
-  const { cta } = contact;
-  const [state, setState] = React.useState({});
-  const handleSubmit = e => {
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: '', email: '', message: '' };
+  }
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  handleSubmit = e => {
     e.preventDefault();
     const form = e.target;
     fetch('/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: new FormData(e.target),
+      body: encode({ 'form-name': 'contact', ...this.state }),
     })
       .then(() => navigate(form.getAttribute('action')))
       .catch(error => alert(error));
   };
 
-  return (
-    <section id="contact">
-      <Container>
-        <Title title="Contact" />
-        <Fade bottom duration={1000} delay={800} distance="30px">
-          <div className="contact-wrapper">
-            <p className="contact-wrapper__text">
-              {cta || 'Would you like to work with me? Awesome!'}
-            </p>
-            <form
-              name="contact"
-              method="POST"
-              data-netlify="true"
-              data-netlify-honeypot="bot-field"
-              action="/"
-              onSubmit={handleSubmit}
-            >
-              <input type="hidden" name="form-name" value="contact" />
-              <p>
-                <label>Your Name</label>
-              </p>
-              <p>
-                <label>
-                  <input type="text" name="name" style={{ width: '300px' }} />
-                </label>
-              </p>
-              <p>
-                <label>Your Email</label>
-              </p>
-              <p>
-                <label>
-                  <input type="email" name="email" style={{ width: '300px' }} />
-                </label>
-              </p>
-              <p>
-                <label>Message</label>
-              </p>
-              <p>
-                <label>
-                  <textarea name="message" style={{ width: '300px' }} rows="6" />
-                </label>
-              </p>
-              <p>
-                <button className="cta-btn" type="submit">
-                  Let's Talk
-                </button>
-              </p>
-            </form>
-          </div>
-        </Fade>
-      </Container>
-    </section>
-  );
-};
+  render() {
+    return (
+      <section id="contact">
+        <Container>
+          <Title title="Contact" />
+          <Fade bottom duration={1000} delay={800} distance="30px">
+            <div className="contact-wrapper">
+              <p className="contact-wrapper__text">Would you like to work with me? Awesome!</p>
+              <form
+                name="contact"
+                method="POST"
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+                action="/"
+                onSubmit={this.handleSubmit}
+              >
+                <input type="hidden" name="form-name" value="contact" />
+                <p>
+                  <label>Your Name</label>
+                </p>
+                <p>
+                  <label>
+                    <input type="text" name="name" style={{ width: '300px' }} />
+                  </label>
+                </p>
+                <p>
+                  <label>Your Email</label>
+                </p>
+                <p>
+                  <label>
+                    <input type="email" name="email" style={{ width: '300px' }} />
+                  </label>
+                </p>
+                <p>
+                  <label>Message</label>
+                </p>
+                <p>
+                  <label>
+                    <textarea name="message" style={{ width: '300px' }} rows="6" />
+                  </label>
+                </p>
+                <p>
+                  <button className="cta-btn" type="submit">
+                    Let's Talk
+                  </button>
+                </p>
+              </form>
+            </div>
+          </Fade>
+        </Container>
+      </section>
+    );
+  }
+}
 
 export default Contact;
